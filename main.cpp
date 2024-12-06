62a0e9b7d5be2ff4eed0f371ca5d42ed55a62887
diego breeg <diegobreeg@gmail.com>
2024-12-06 09:40:12 -0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <format>
+
+int main()
+{
+    int apples{ 5 };
+
+    std::cout
+        << "I have "
+        << apples
+        << " apples";
+
+    return 0;
+}
